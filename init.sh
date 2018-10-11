36a6ad034fb54ecb52060cfdebb88f8e54aa0906
John Brosnan <john@thepond.com>
2018-10-11 11:38:26 -0400

    add command line edit

--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@ export ENHANCD_HYPHEN_ARG=_
 
 if $(which rbenv >/dev/null 2>&1); then eval "$(rbenv init -)"; fi
 set -m 
+
+
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd "\\" edit-command-line
